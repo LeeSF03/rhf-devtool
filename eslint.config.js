4ccdcb79f6d8a5648ab7a327060c02b18c160231
LeeSF03 <shuenfeilee@gmail.com>
2026-03-12 17:19:04 +0800

    feat: add eslint-react and react-hook plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 import eslint from "@eslint/js"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
 import { defineConfig, globalIgnores } from "eslint/config"
 import tseslint from "typescript-eslint"
 
 const eslintConfig = defineConfig([
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  react.configs.flat["jsx-runtime"],
+  reactHooks.configs.flat.recommended,
   globalIgnores(["dist/**", "node_modules/**"]),
 ])
 
